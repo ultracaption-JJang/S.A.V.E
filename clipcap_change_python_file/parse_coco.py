c4686a06a9449f09869033f762fa67b5e5e3bd35
SeongjaeP <psjj960507@gmail.com>
2024-10-30 15:39:46 +0900

    feat: CLIP용으로

--- clipcap_change_python_file/parse_coco.py
+++ clipcap_change_python_file/parse_coco.py
@@ -12,9 +12,9 @@ import argparse
 def main(clip_model_type: str):
     device = torch.device('cuda:0')
     clip_model_name = clip_model_type.replace('/', '_')
-    out_path = f"/Users/psjj/S.A.V.E/oscar_split_{clip_model_name}_train.pkl"
-    clip_model, preprocess = clip.load(clip_model_type, device=device, jit=False)
-    with open('/Users/psjj/Downloads/coco2017/filtered_captions.json', 'r') as f:
+    out_path = f"/Users/psjj/S.A.V.E/only_clip_{clip_model_name}_train.pkl"
+    clip_model, preprocess = clip.load('your_path', device=device, jit=False)
+    with open('/Users/psjj/Downloads/coco2017/stop_sign_captions.json', 'r') as f:
         data = json.load(f)
     print("%0d captions loaded from json " % len(data))
     all_embeddings = []
@@ -22,7 +22,7 @@ def main(clip_model_type: str):
     for i in tqdm(range(len(data))):
         d = data[i]
         img_id = d["image_id"]
-        filename = f"/Users/psjj/Downloads/coco2017/filtered_images_{int(img_id):012d}.jpg"
+        filename = f"/Users/psjj/Downloads/coco2017/stop_sign_images_{int(img_id):012d}.jpg"
         # if not os.path.isfile(filename):
         #     filename = f"./data/coco/val2014/COCO_val2014_{int(img_id):012d}.jpg"
         image = io.imread(filename)
